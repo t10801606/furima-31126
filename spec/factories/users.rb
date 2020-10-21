8dc29a72c5a8009246c2d50d686dfe2e86f485a8
M.Ishii <t10801606@gunma-u.ac.jp>
2020-10-21 14:18:43 +0900

    Rubocopによる修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,15 +1,14 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.name}
-    email                 {Faker::Internet.free_email}
+    nickname              { Faker::Name.name }
+    email                 { Faker::Internet.free_email }
     password = Faker::Internet.password(min_length: 6)
-    password              {password}
-    password_confirmation {password}
-    first_name            {Faker::Name.first_name}
-    last_name             {Faker::Name.last_name}
-    first_name_kana       {"test"}
-    last_name_kana        {"test"}
-    birth_day {Faker::Date.between(from: '1930-01-01', to: '2015-12-31')}
-
+    password              { password }
+    password_confirmation { password }
+    first_name            { Faker::Name.first_name }
+    last_name             { Faker::Name.last_name }
+    first_name_kana       { 'test' }
+    last_name_kana        { 'test' }
+    birth_day { Faker::Date.between(from: '1930-01-01', to: '2015-12-31') }
   end
 end
