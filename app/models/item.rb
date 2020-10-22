65b0c80cd7950d5a4c25008ea6e9b7ee9bcf129b
M.Ishii <t10801606@gunma-u.ac.jp>
2020-10-22 12:42:57 +0900

    画像のバリデーション、アソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :explain
+    validates :image
     validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, format: {with: /\A[0-9]+\z/}
     # 半角数字
     with_options numericality: { other_than: 1 } do
@@ -20,6 +21,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :charge
   belongs_to_active_hash :area
   belongs_to_active_hash :delivery_date
+  belongs_to_active_hash :category
   belongs_to_active_hash :status
-  belongs_to_active_hash :status
+  has_one_attached :image
 end
