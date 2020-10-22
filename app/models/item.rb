148b7c4573395a5e71024c93280b7bce3d438d1b
M.Ishii <t10801606@gunma-u.ac.jp>
2020-10-22 18:25:13 +0900

    発送までの日数に関するActivehashの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,9 +19,9 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :buyer
   has_one_attached :image
-  # belongs_to_active_hash :charge
-  # belongs_to_active_hash :area
-  # belongs_to_active_hash :delivery_date
+  belongs_to_active_hash :charge
+  belongs_to_active_hash :area
+  belongs_to_active_hash :delivery_date
   belongs_to_active_hash :category
   belongs_to_active_hash :status
 end
