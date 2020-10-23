2b58170b2bc1ab964f6f27fefb89f468e747b050
M.Ishii <t10801606@gunma-u.ac.jp>
2020-10-23 10:58:17 +0900

    テスト項目およびダミーデータのシングルクォーテーション削除

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :item do
     name             { Faker::Name.name }
     explain          { '2010年頃に1万円で購入したジャケットです。ライトグレーで傷はありません。あわせやすいのでおすすめです。' }
-    charge_id        { '2' }
-    area_id          { '2' }
-    delivery_date_id { '2' }
-    price            { '1000' }
-    category_id      { '2' }
-    status_id        { '2' }
+    charge_id        { 2 }
+    area_id          { 2 }
+    delivery_date_id { 2 }
+    price            { 1000 }
+    category_id      { 2 }
+    status_id        { 2 }
     association :user
     
     after(:build) do |item|
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -29,31 +29,31 @@ RSpec.describe Item, type: :model do
     end
 
     it 'カテゴリーの情報が含まれない(idが1の場合)と登録できないこと' do
-      @item.category_id = '1'
+      @item.category_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Category must be other than 1")
     end
 
     it '商品の状態についての情報が含まれない(idが1の場合)と登録できないこと' do
-      @item.status_id = '1'
+      @item.status_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Status must be other than 1")
     end
 
     it '配送料の負担についての情報が含まれない(idが1の場合)と登録できないこと' do
-      @item.charge_id = '1'
+      @item.charge_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Charge must be other than 1")
     end
 
     it '発送元の地域についての情報が含まれない(idが1の場合)と登録できないこと' do
-      @item.area_id = '1'
+      @item.area_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Area must be other than 1")
     end
 
     it '発送までの日数についての情報が含まれない(idが1の場合)と登録できないこと' do
-      @item.delivery_date_id = '1'
+      @item.delivery_date_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Delivery date must be other than 1")
     end
