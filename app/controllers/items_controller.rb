096e32edceaa01bc2bbbdca0344fb1b7125e23ab
M.Ishii <t10801606@gunma-u.ac.jp>
2020-10-23 19:37:46 +0900

    ユーザーアクセス範囲の制限の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_to_index, only: [:edit]
   def index
     @items = Item.includes(:user).order('created_at DESC')
     @buyers = Buyer.all
@@ -35,5 +36,12 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :explain, :charge_id, :area_id, :delivery_date_id, :price, :category_id, :status_id, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 end
