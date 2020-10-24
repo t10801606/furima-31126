79c551c1c7ac5d4399968561bf9a6da530ed53ba
M.Ishii <t10801606@gunma-u.ac.jp>
2020-10-24 17:22:38 +0900

    エラーメッセージの出力設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,6 +30,11 @@ class ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     @item.update(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
