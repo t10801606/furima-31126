2749b04d0c8495dfa6dc105bb6a96456b41b5984
M.Ishii <t10801606@gunma-u.ac.jp>
2020-10-28 09:23:45 +0900

    商品購入ページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'acceptances/index'
   devise_for :users
   root to: "items#index"
-  resources :items, only: [:new, :create, :show, :update, :edit, :destroy]
+  resources :items, only: [:new, :create, :show, :update, :edit, :destroy] do
+    resources :buyers, only: [:index, :create]
+  end
 end
