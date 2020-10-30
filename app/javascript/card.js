abb702f26f4d430941a139073f1f5c141935bc2a
M.Ishii <t10801606@gunma-u.ac.jp>
2020-10-30 11:09:36 +0900

    クレジットカードのフォーム情報を削除する処理の追加

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -20,10 +20,10 @@ const pay = () => {
         const tokenObj = `<input value=${token} name='token' type="hidden"> `;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
-      // document.getElementById("user_purchase_number").removeAttribute("name");
-      // document.getElementById("user_purchase_cvc").removeAttribute("name");
-      // document.getElementById("user_purchase_exp_month").removeAttribute("name");
-      // document.getElementById("user_purchase_exp_year").removeAttribute("name");
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
 
       document.getElementById("charge-form").submit();
     });
